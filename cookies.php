56a73308afe872fc9ff48612b68744afa9dc1b7b
ramazancetinkaya <75214035+ramazancetinkaya@users.noreply.github.com>
2022-06-12 12:44:42 +0300

    Create cookies.php

---
+++ cookies.php
@@ -0,0 +1,14 @@
+<?php
+
+// UNSET COOKIES
+if (isset($_SERVER['HTTP_COOKIE'])) {
+    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+    foreach($cookies as $cookie) {
+        $parts = explode('=', $cookie);
+        $name = trim($parts[0]);
+        setcookie($name, '', time()-1000);
+        setcookie($name, '', time()-1000, '/');
+    }
+}
+
+?>
